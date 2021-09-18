2e98bc3cda2702995a6ef1e0acf001c640785183
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2021-09-18 08:30:47 -0500

    .

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  name: { type: String, required: true, min: 6, max: 255 },
-  email: { type: String, require: true, unique: true, min: 6, max: 255 },
-  password: { type: String, required: true, min: 6, max: 1024 },
+  name: { String, required: true, min: 6, max: 255 },
+  email: { String, require: true, unique: true, min: 6, max: 255 },
+  password: { String, required: true, min: 6, max: 1024 },
   date: { Date, default: Date.now },
 });
 
